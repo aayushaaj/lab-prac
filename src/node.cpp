b3d661e420c3f15ef48f501ae99fdfb871275766
RiCE <aayushajaspau@gmail.com>
2024-04-10 10:52:34 +0545

    ...

--- src/node.cpp
+++ src/node.cpp
@@ -1,33 +1,53 @@
 #include "linkedlist.h"
 
-void linkedlist::addtohead(int data){
-    node *newnode =new node(data, HEAD);
-    newnode->info=data;
-    newnode->next=HEAD;
-    HEAD=newnode;
-      
-}
+void linkedlist::addtohead(int data)
+    {
+        node *newnode =new node(data, HEAD);
+        newnode->info=data;
+        newnode->next=HEAD;
+        HEAD=newnode;    
+    }
 void linkedlist::add(node *pred,int data)
-{   node *newnode;
-    newnode->info=data;
-    newnode->next=pred->next;
-    pred->next=newnode;
-}
+    {   node *newnode;
+        newnode->info=data;
+        newnode->next=pred->next;
+        pred->next=newnode;
+    }
 void linkedlist::addtotail(int data)
-{
-    node *newnode;
-    newnode->info=data;
-    newnode->next=nullptr;
-    TAIL->next=newnode;
-    TAIL=TAIL->next;
-}
+    {
+        node *newnode;
+        newnode->info=data;
+        newnode->next=nullptr;
+        TAIL->next=newnode;
+        TAIL=TAIL->next;
+    }
 
-    bool linkedlist::remove(int data){
+bool linkedlist::remove(int data)
+    {
+        if(isempty())
+        {
+
+        }
+        else{
+
+        }
+
+    }
+bool linkedlist::removeFromHead(int &data)
+    {
+
+    }
+bool linkedlist::removeFromTail(int &data)
+    {
+
+    }
+
+bool linkedlist::isempty()
+    {
         
     }
-    bool linkedlist::removeFromHead(int &data){}
-    bool linkedlist::removeFromTail(int &data){}
 
-    bool linkedlist::isempty(){}
-
-    void linkedlist::head(int &data){}
+void linkedlist::head(int &data)
+    {
+        
+    }
